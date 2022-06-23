9470fe53f958b9ffee2e47f24f2e2ece3464b6b9
rafaelpcosta1 <rpdc.rafael@gmail.com>
2022-06-23 13:15:57 -0300

    feat: add login page and form

--- src/app/pages/login/Login.tsx
+++ src/app/pages/login/Login.tsx
@@ -1,5 +1,30 @@
+import { useState } from "react";
+
 export const Login = () => {
+    //Criando STATE = USESTATE
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    //Criando Funcões Arrow
+    const handleEntrar = () => {
+        console.log(email);
+        console.log(password);
+    }
+
+    //retornando os elementos da pagina Login
     return (
-        <h1>Pagina de Login</h1>
+        <div>
+            <form>
+                <label>
+                    <span>Email: </span>
+                    <input value={email} onChange={e => setEmail(e.target.value)} />
+                </label>
+                <label>
+                    <span>Senha: </span>
+                    <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
+                </label>
+                <button type="button" onClick={handleEntrar}>Entrar</button>
+            </form>
+        </div>
     );
 }
