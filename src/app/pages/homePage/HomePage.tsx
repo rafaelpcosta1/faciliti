4093d918d3c97a57cc43ab81d967f30724bec188
rafaelpcosta1 <rpdc.rafael@gmail.com>
2022-06-23 19:51:54 -0300

    feat: add homePage using useRef without render

--- src/app/pages/homePage/HomePage.tsx
+++ src/app/pages/homePage/HomePage.tsx
@@ -1,9 +1,19 @@
+import { useRef } from "react";
 import { Link } from "react-router-dom";
 
 export const HomePage = () => {
+    const counterRef = useRef({ counter: 0 });
+
+    //Usando useRef para armazenar valores sem renderizar na tela
     return (
         <div>
             <p>Pagina Inicial</p>
+
+            <p>Contador: {counterRef.current.counter}</p>
+
+            <button onClick={() => counterRef.current.counter++}>Contar</button>
+            <button onClick={() => console.log(counterRef.current.counter)}>Log</button>
+
             <Link to={"/login"}>Login</Link>
         </div>
     )
