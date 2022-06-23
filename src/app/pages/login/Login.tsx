5be0238ea5f1787bed585684bb0ba93d9282ca7d
rafaelpcosta1 <rpdc.rafael@gmail.com>
2022-06-23 19:37:28 -0300

    feat: add login using useRef

--- src/app/pages/login/Login.tsx
+++ src/app/pages/login/Login.tsx
@@ -1,6 +1,8 @@
-import { useCallback, useMemo, useState } from "react";
+import { useCallback, useMemo, useRef, useState } from "react";
 
 export const Login = () => {
+    const inputPasswordRef = useRef<HTMLInputElement>(null);
+
     //Criando STATE = UseState
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
@@ -14,6 +16,9 @@ export const Login = () => {
     const handleEntrar = useCallback(() => {
         console.log(email);
         console.log(password);
+        if (inputPasswordRef.current !== null) {
+            inputPasswordRef.current.focus();
+        }
     }, [email, password]); //Importante colocar os states no Array de dependencias ao usar o useCallback, a não utilização do array pode trazer problemas.
 
     //retornando os elementos da pagina Login
@@ -24,11 +29,20 @@ export const Login = () => {
 
                 <label>
                     <span>Email: </span>
-                    <input value={email} onChange={e => setEmail(e.target.value)} />
+                    <input
+                        value={email}
+                        onChange={e => setEmail(e.target.value)}
+                        onKeyDown={e => e.key === 'Enter' ? inputPasswordRef.current?.focus() : undefined}
+                    />
                 </label>
                 <label>
                     <span>Senha: </span>
-                    <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
+                    <input
+                        type="password"
+                        value={password}
+                        ref={inputPasswordRef}
+                        onChange={e => setPassword(e.target.value)}
+                    />
                 </label>
                 <button type="button" onClick={handleEntrar}>Entrar</button>
             </form>
