e42c9efb456f5e015cfc4751c30c88112355b4ec
rafaelpcosta1 <rpdc.rafael@gmail.com>
2022-06-29 00:13:09 -0300

    feat: create service access to backend (API)

---
+++ src/app/shared/services/api/tarefas/TarefasService.ts
@@ -0,0 +1,66 @@
+import { Api } from "../ApiConfig";
+import { ApiException } from "../ApiException";
+
+interface ITarefa {
+   id: number;
+   title: string;
+   isCompleted: boolean;
+}
+
+const create = async (dataToCreate: Omit<ITarefa, 'id'>): Promise<ITarefa | ApiException> => {
+   try {
+      const { data } = await Api().post<any>('/tarefas', dataToCreate)
+      return data;
+   }
+   catch (error: any) {
+      return new ApiException(error.message || 'Erro ao incluir o registro.')
+   }
+};
+
+const getAll = async (): Promise<ITarefa[] | ApiException> => {
+   try {
+      const { data } = await Api().get('/tarefas');
+      return data;
+   }
+   catch (error: any) {
+      return new ApiException(error.message || 'Erro ao buscar os registros.')
+   }
+};
+
+const getById = async (id: number): Promise<ITarefa | ApiException> => {
+   try {
+      const { data } = await Api().get(`/tarefas/${id}`);
+      return data;
+   }
+   catch (error: any) {
+      return new ApiException(error.message || 'Erro ao buscar o registro.')
+   }
+};
+
+const updateById = async (id: string, dataToUpdate: ITarefa): Promise<ITarefa | ApiException> => {
+   try {
+      const { data } = await Api().put(`/tarefas/${id}`, dataToUpdate);
+      return data;
+   }
+   catch (error: any) {
+      return new ApiException(error.message || 'Erro ao alterar o registro.');
+   }
+};
+
+const deleteById = async (id: string): Promise<undefined | ApiException> => {
+   try {
+      await Api().get(`/tarefas/${id}`);
+      return undefined;
+   }
+   catch (error: any) {
+      return new ApiException(error.message || 'Erro ao apagar o registro.')
+   }
+};
+
+export const TarefasService = {
+   create,
+   getAll,
+   getById,
+   updateById,
+   deleteById,
+};
