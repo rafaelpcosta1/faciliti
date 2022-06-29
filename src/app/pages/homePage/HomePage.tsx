3abd9fbf69b9a8dacdc96a5369e7de4c687c71fa
rafaelpcosta1 <rpdc.rafael@gmail.com>
2022-06-29 00:43:40 -0300

    feat: get tasks in API

--- src/app/pages/homePage/HomePage.tsx
+++ src/app/pages/homePage/HomePage.tsx
@@ -1,13 +1,21 @@
-import { useCallback, useState } from "react";
-
-interface ITarefa {
-    id: number;
-    title: string;
-    isCompleted: boolean;
-}
+import { useCallback, useEffect, useState } from "react";
+import { ApiException } from "../../shared/services/api/ApiException";
+import { ITarefa, TarefasService } from '../../shared/services/api/tarefas/TarefasService';
 
 export const HomePage = () => {
     const [lista, setLista] = useState<ITarefa[]>([]);
+
+    useEffect(() => {
+        TarefasService.getAll()
+            .then((result) => {
+                if (result instanceof ApiException) {
+                    alert(result.message);
+                }
+                else {
+                    setLista(result);
+                }
+            });
+    }, []);
 
     const handleInputKeyDown: React.KeyboardEventHandler<HTMLInputElement> = useCallback((e) => {
         if (e.key === 'Enter') {
